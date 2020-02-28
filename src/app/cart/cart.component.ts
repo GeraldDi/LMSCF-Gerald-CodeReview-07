96b84e42f58226e6df99ba55b55d96c434ee8a8d
GeraldDi <59823625+GeraldDi@users.noreply.github.com>
2020-02-28 16:08:07 +0100

    added functinality

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -10,20 +10,39 @@ import { FormBuilder } from '@angular/forms';
 export class CartComponent implements OnInit {
  items;
  checkoutForm;
+ tprice;
 
  constructor( public cartService: CartService, public formBuilder: FormBuilder) {
          this.checkoutForm = this.formBuilder.group({
      name: '',
-     address: ''
+     address: '',
+     emaila:''
    });
  }
 
  ngOnInit() {
          this.items = this.cartService.getItems();
+         let tprice = 0;
+         for (let i=0;i<this.items.length;i++){
+         tprice += this.items[i].price;
+         console.log("tprice:"+tprice)
+         // this.tprice =tprice;
+     }
+     if (tprice >500){
+     	this.tprice=tprice*0.8;
+     }else if (tprice>200){
+     	this.tprice=tprice*0.9;
+     }else{
+     	this.tprice =tprice;
+     }
+
+
+
  }
  onSubmit(customerData) {
    // Process checkout data here
    console.warn('Your order has been submitted', customerData);
+   window.alert("Thank you for your purchase "+ customerData.name+"! A confirmation mail will be sent to "+customerData.emaila);
 
    this.items = this.cartService.clearCart();
    this.checkoutForm.reset();
--- src/app/travels/travels.component.ts
+++ src/app/travels/travels.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { products } from '../products';
 import { CartService } from '../cart.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-travels',
@@ -11,6 +12,16 @@ export class TravelsComponent implements OnInit {
 	products = products;
 	product;
 
+
+   newloc = new FormGroup({
+     name: new FormControl('',Validators.required),
+     description: new FormControl('',Validators.required),
+     start:new FormControl('',Validators.required),
+     duration:new FormControl('',Validators.required),
+     price: new FormControl('',Validators.required),
+     // img: new FormControl('',Validators.required),
+   });
+
   constructor(public cartService: CartService) { }
 
    addToCart(product) {
@@ -20,5 +31,12 @@ export class TravelsComponent implements OnInit {
 
   ngOnInit(): void {
   }
+    onSubmit(){
 
+    if(this.newloc.valid){
+      var a = this.newloc.value;
+      console.log(a)
+      this.products.push(a);
+   }
+  }
 }
